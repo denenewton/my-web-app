63ac854a9b4c96480a53878128ab7dbb9489a56e
Daniel Santos <denenewton@icloud.com>
2024-10-18 22:03:37 -0300

    add other url

--- src/pages/SingleMovies.tsx
+++ src/pages/SingleMovies.tsx
@@ -11,7 +11,7 @@ const SingleMovies = () => {
 
   async function populateMoviesData(id:any) {
     setLoading(true)
-    const response = await fetch('https://denenewton.netlify.app/pages/movies-data.json');
+    const response = await fetch('https://github.com/denenewton/my-web-app/blob/master/src/pages/movies-data.json');
     const data = await response.json();
     const movie: MovieObj = data.find((m: { _id: string }) => m._id == id);
     
