3c3581a1a0115f3a219f776471a0784e5044141d
Daniel Santos <denenewton@icloud.com>
2024-10-18 22:23:39 -0300

    mudei json para ./

--- src/contex/SearchContext.tsx
+++ src/contex/SearchContext.tsx
@@ -32,7 +32,7 @@ export function SearchProvider({ children }:Props) {
   const [movies, setMovies] = useState<MovieObj[]>([]);
 
   async function getMovies() {
-    const response = await fetch('https://github.com/denenewton/my-web-app/blob/master/src/pages/movies-data.json');
+    const response = await fetch('https://github.com/denenewton/my-web-app/movies-data.json');
     const data: MovieObj[] = await response.json();
     setMovies(data);
   }
